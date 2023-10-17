1920abc9ff2df8d0613f6541346f15b63ba3db63
Jose Gabriel <jgabrielfd0+github@gmail.com>
2023-10-16 22:39:22 -0300

    Criando a rota '/users'

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,60 @@
 import express, { Request, Response } from "express";
+import { v4 as uuidv4 } from "uuid";
+
+export type Technologie = {
+  id: string;
+  title: string;
+  studied: boolean;
+  deadline: Date;
+  created_at: Date;
+};
+
+export type User = {
+  id: string;
+  name: string;
+  username: string;
+  technologies: Technologie[];
+};
+
+const users = [] as User[];
 
 const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  return res.send("<h1>Teste</h1>");
+app.use(express.json());
+
+app.post("/users", (req, res) => {
+  const { name, username } = req.body;
+
+  const newUser: User = {
+    name,
+    username,
+    id: uuidv4(),
+    technologies: [],
+  };
+  users.push(newUser);
+  return res.status(201).json(newUser);
 });
+
+// app.get("/technologies", (req: Request, res: Response) => {
+//   return;
+// });
+
+// app.post("/technologies", (req: Request, res: Response) => {
+//   return;
+// });
+
+// app.put("/technologies/:id", (req: Request, res: Response) => {
+//   return;
+// });
+
+// app.patch("/technologies/:id/studied", (req: Request, res: Response) => {
+//   return;
+// });
+
+// app.delete("/technologies/:id", (req: Request, res: Response) => {
+//   return;
+// });
 
 app.listen(PORT, () => console.log(`Server is Running on port: ${PORT}`));
